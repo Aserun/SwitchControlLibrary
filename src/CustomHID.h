ac4b04ab78a61b9d337090cd49cc509d44f9b692
celclow <celclow@gmail.com>
2020-01-26 12:21:28 +0900

    editorconfigを追加する。
    ともない、フォーマッタの再実行を行う。

--- src/CustomHID.h
+++ src/CustomHID.h
@@ -5,8 +5,8 @@
 class CustomHID_ : public HID_
 {
 public:
-  CustomHID_();
-  int SendReport(const void *data, int len);
+    CustomHID_();
+    int SendReport(const void *data, int len);
 };
 
 CustomHID_ &CustomHID();
